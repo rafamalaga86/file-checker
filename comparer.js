a57166d02cc34e93781c76cb86efc8ddb456e73a
Rafael <rafamalaga86@gmail.com>
2024-11-08 19:04:54 -0500

    Improved Comparer

--- comparer.js
+++ comparer.js
@@ -1,5 +1,6 @@
 const { getById } = require('./models/checksum');
 const { consoleLog } = require('./loggers');
+const { printYellow, printGreen, printBlue, printRed, eol, print } = require('./loggers');
 
 function receiveArguments() {
   const id1 = process.argv[2];
@@ -20,14 +21,37 @@ async function main() {
   const filtered1 = process1Stringified.filter(x => !process2Stringified.includes(x));
   const filtered2 = process2Stringified.filter(x => !process1Stringified.includes(x));
 
-  consoleLog(`Process ${id1} has ${process1.length} files.`);
-  consoleLog(`Process ${id2} has ${process2.length} files.`);
-  consoleLog(
-    `Process ${id1} has ${filtered1.length} files that are not in process ${id2}`
-  );
-  consoleLog(
-    `Process ${id2} has ${filtered2.length} files that are not in process ${id1}`
-  );
+  print('Process ');
+  printYellow(id1);
+  print(' has ');
+  printGreen(process1.length + ' ');
+  print('files');
+  eol();
+
+  print('Process ');
+  printYellow(id2);
+  print(' has ');
+  printGreen(process2.length + ' ');
+  print('files');
+  eol();
+  eol();
+
+  print('Process ');
+  printYellow(id1);
+  print(' has ');
+  printGreen(filtered1.length);
+  print(' files that are not in process ');
+  printYellow(id2);
+  eol();
+
+  print('Process ');
+  printYellow(id2);
+  print(' has ');
+  printGreen(filtered2.length);
+  print(' files that are not in process ');
+  printYellow(id1);
+  eol();
+
   process.exit(0);
 }
 
